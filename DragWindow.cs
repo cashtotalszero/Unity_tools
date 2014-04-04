6ddd918a6821ced31a44206b2bd6a00dc0775e4a
Alex <paznoz@gmail.com>
2014-04-04 14:44:17 +0100

    DragWindow UnityAPI example

---
+++ DragWindow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragWindow : MonoBehaviour {
+
+	public Rect windowRect = new Rect(20, 20, 120, 50);
+	void OnGUI() {
+		windowRect = GUI.Window(0, windowRect, DoMyWindow, "My Window");
+	}
+	void DoMyWindow(int windowID) {
+		GUI.DragWindow(new Rect(0, 0, 10000, 20));
+	}
+}
