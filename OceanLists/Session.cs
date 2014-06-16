77e4fc9b5c23c337e1ef24d2861d6500997c0dac
Alex <paznoz@gmail.com>
2014-06-16 16:18:55 +0100

    Amended session class

--- OceanLists/Session.cs
+++ OceanLists/Session.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Session {
 
+	public int ID { get; set;}				// The unique ID of the session (matches index number)
 	public int Cursor { get; set;}			// The Molecule index of the cursor position in the Ocean
 	public int Ocean { get; set;}			// The oceanList index of the Ocean being referenced by the cursor
 	public int Privileges { get; set;}		// Read/Write access for the session	
