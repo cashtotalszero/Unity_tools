973db5451c939e9a4ab0d47f04f23eb27d457eb4
Alex <paznoz@gmail.com>
2014-09-18 16:08:51 +0100

    .

---
+++ FINAL/Assets/Molecule.cs
@@ -0,0 +1,25 @@
+﻿/* 
+Gamification of Space Exploration Project.
+
+Written in 2014 by Alex Parrott
+
+To the extent possible under law, the author(s) have dedicated all
+copyright and related and neighboring rights to this software to the
+public domain worldwide. This software is distributed without any
+warranty.
+
+You should have received a copy of the CC0 Public Domain Dedication
+along with this software. If not, see
+<http://creativecommons.org/publicdomain/zero/1.0/>.
+*/
+
+using UnityEngine;
+using System.Collections;
+
+public class Molecule {
+
+	public string Name { get; set;}
+	public string Type { get; set;}
+	public object Value { get; set;}
+	public object Data { get; set;}
+}
